b0d3fb831c2dd1a773a2965b084f0e3220deda7a
koryagin2006 <53011800+koryagin2006@users.noreply.github.com>
2020-01-02 17:27:07 +0500

    Приведение типов

--- 1.2. Переменные. Типы данных. Преобразование типов.py
+++ 1.2. Переменные. Типы данных. Преобразование типов.py
@@ -1,9 +1,9 @@
 # верно и типы данных
-person_name = 'Max' # <class 'str'>
-age = 30 # <class 'int'>
+person_name = 'Max'  # <class 'str'>
+age = 30  # <class 'int'>
 period = 3.2  # <class 'float'>
-is_good = True # <class 'bool'>
-person = None # <class 'NoneType'>
+is_good = True  # <class 'bool'>
+person = None  # <class 'NoneType'>
 # неверно
 pn = 'Max'
 a = 30
@@ -12,3 +12,15 @@ q = True
 pp = None
 
 # Приведение типов
+birthday_year = '1988'
+print(type(birthday_year))
+
+period = 20
+print(type(period))
+
+# age = birthday_year + period # -> TypeError: can only concatenate str (not "int") to str
+age = int(birthday_year) + period  # математическое сложение
+print(age)
+
+some_str = birthday_year + str(period)  # конкатенация
+print(some_str)
