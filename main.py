df76087412ae19f75fcf29a0d7f5eed8cd12a881
koryagin2006 <53011800+koryagin2006@users.noreply.github.com>
2020-01-02 13:53:52 +0500

    Update main.py
    Продолжение

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
 print('Hello, world!')
+
+'''
+a =\
+    int(input())
+if a < -5:
+    print('Low')
+elif -5 <= a <= 5:
+    print('Mid')
+else:
+    print('High')
+'''
+
+i = 5
+while i < 15:
+    print(i)
+    i = i + 2
+for i in 'hello world':
+    print(i*2, end='')
