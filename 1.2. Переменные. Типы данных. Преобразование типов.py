e0db1e0ecde1ab410a23841b7f8a2b7e809badbb
koryagin2006 <53011800+koryagin2006@users.noreply.github.com>
2020-01-02 17:11:37 +0500

    Урок 1.2 Переменные и типы
    Начало...

---
+++ 1.2. Переменные. Типы данных. Преобразование типов.py
@@ -0,0 +1,14 @@
+# верно и типы данных
+person_name = 'Max' # <class 'str'>
+age = 30 # <class 'int'>
+period = 3.2  # <class 'float'>
+is_good = True # <class 'bool'>
+person = None # <class 'NoneType'>
+# неверно
+pn = 'Max'
+a = 30
+p = 3.2
+q = True
+pp = None
+
+# Приведение типов
