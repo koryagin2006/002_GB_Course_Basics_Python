1945bf617f61df4d34201ffb307c924e2103218b
koryagin2006 <53011800+koryagin2006@users.noreply.github.com>
2020-01-04 02:49:32 +0500

    Create 2.1 Строки. Определение. Методы.py
    Срезы

---
+++ 2. Встроенные типы/2.1 Строки. Определение. Методы.py
@@ -0,0 +1,10 @@
+friend = 'Максим'
+print(friend)  # <class 'str'>
+print('say "Hello"')
+print('Первая бука имени друга', friend[0])
+print('Предпоследняя бука имени друга', friend[-2])
+
+# срезы
+print(friend[1:4])  # - c второго по третий символ
+print(friend[:4])  # - c начала строки по третий символ
+print(friend[3:])  # - c четветрого по конец строки
