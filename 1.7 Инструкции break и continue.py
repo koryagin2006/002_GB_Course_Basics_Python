d004338172ad2515a0bb1bfb735c8cf5cde720c7
koryagin2006 <53011800+koryagin2006@users.noreply.github.com>
2020-01-03 19:21:53 +0500

    Create 1.7 Инструкции break и continue.py

---
+++ 1.7 Инструкции break и continue.py
@@ -0,0 +1,29 @@
+# 1
+name = None
+
+while True:
+    name = input('Кто создатель python? ')
+    if name == 'Гвидо':
+        break
+    print("Неверно")
+
+print('Верно')
+
+# 2
+# вывод четных чисел от 0 до n
+number = 0
+n = int(input('Введите n: '))
+
+while number <= n:
+    if number % 2 == 0
+        number += 1
+        continue
+    print(number)
+    number += 1
+
+number = 0
+while number <= 100:
+    print(number)
+    number += 1
+else:
+    print('end')
