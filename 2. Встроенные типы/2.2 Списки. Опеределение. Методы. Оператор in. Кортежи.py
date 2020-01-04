8ce3d0b2a82b6378895894477ff4402e98f9881a
Andrey Koryagin <53011800+koryagin2006@users.noreply.github.com>
2020-01-04 13:53:32 +0500

    Create 2.2 Списки. Опеределение. Методы. Оператор in. Кортежи.py
    начало

---
+++ 2. Встроенные типы/2.2 Списки. Опеределение. Методы. Оператор in. Кортежи.py
@@ -0,0 +1,14 @@
+# Тип данных list
+
+empty_list = []  # можно объявить пустой список
+friends = ['Max', 'Leo', 'Kate']  # можно сразу заполнить список элементарми
+print(type(friends))  # <class 'list'>
+print('Второй друг', friends[1])  # Доступны индексы
+print('Первый друг с конца', friends[-1])  # Доступны индексы
+print(len(friends))  # количество элементов, в данном случае - друзей
+
+friends.append('Ron')  # добавляет элемент
+print(friends, len(friends))
+
+print(friends.pop())  # удаляет последний элемент + выводит его на экран
+print(friends)
