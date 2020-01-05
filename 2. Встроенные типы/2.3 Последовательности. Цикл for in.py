007678a5d2d7c54a2cedf932194496f9c0eb513f
Andrey Koryagin <53011800+koryagin2006@users.noreply.github.com>
2020-01-06 00:27:05 +0500

    Update 2.3 Последовательности. Цикл for in.py

--- 2. Встроенные типы/2.3 Последовательности. Цикл for in.py
+++ 2. Встроенные типы/2.3 Последовательности. Цикл for in.py
@@ -1,3 +1,31 @@
 # Последовательность  - контейнер, элементы которого представляют собой некую коследовательность.
 # Могут быть изменяемыми - список, так и неизменяемыми - кортеж или строка
 
+friend_name = 'Max'
+friends = ['Max', 'Leo', 'Kate']
+roles = ('admin', 'guest', 'user')
+
+if 'Max' in friends:
+    print('У меня есть этот друг')
+if 'M' in friend_name:
+    print('Буква М есть в имени друга')
+
+# while
+i = 0
+while i < len(friends):
+    friend = friends[i]
+    print(friend)
+    i += 1
+print('--------')
+# for
+for friend in friends:
+    print(friend)
+
+for letter in friend_name:
+    print(letter)
+
+for role in roles:
+    print(role)
+
+print('--------\nend')
+
