00889bc8a7bd259c25a92eefabb93f75fc483390
koryagin2006 <53011800+koryagin2006@users.noreply.github.com>
2020-01-08 12:15:37 +0500

    Добавление количества попыток и уровней сложности

--- 3. Практикум. Игра «Угадай число»/3.2 Добавление уровня сложности.py
+++ 3. Практикум. Игра «Угадай число»/3.2 Добавление уровня сложности.py
@@ -4,10 +4,21 @@ import random
 number = random.randint(1, 100)
 # print(number)
 user_number = None
+count = 0  # введем количество попыток
+levels = {1: 10, 2: 5, 3: 3}  # уровни сложности
+level = int(input('Выберите уроверь сложности 1-3: '))
+max_count = levels[level]  # максимально количество попыток
+
 while number != user_number:
+    count += 1
+    if count > max_count:
+        print('Вы проиграли')
+        break
+    print(f'Попытка № {count}')
     user_number = int(input('Введите число: '))
     if number < user_number:
         print('Ваше число больше загаданного')
     else:
         print('Ваше число меньше загаданного')
-print('Победа!')
+else:
+    print('Победа!')
