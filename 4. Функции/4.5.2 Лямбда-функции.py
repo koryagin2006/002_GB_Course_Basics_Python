67cdb059c72d1b88c894405d006f0742c6a64a8f
koryagin2006 <53011800+koryagin2006@users.noreply.github.com>
2020-01-14 00:47:01 +0500

    Update 4.5.2 Лямбда-функции.py
    Дополнение

--- 4. Функции/4.5.2 Лямбда-функции.py
+++ 4. Функции/4.5.2 Лямбда-функции.py
@@ -1,3 +1,27 @@
 # lambda-функции
 # применяются для создания анонимных функций по месту их использования
-# lambda входные параметры: результат
+# Синтаксис:
+# lambda <входные параметры> : <результат>
+numbers = [1, 2, 3, 4, 5, 6, 7, 8]
+
+
+def my_filter(numbers, function):
+    result = []
+    for number in numbers:
+        if function(number):
+            result.append(number)
+    return result
+
+
+# def is_even(number):
+#     return number % 2 == 0
+
+# для поиска четных чисел
+
+print(my_filter(numbers=numbers, function=lambda number: number % 2 == 0))  # [2, 4, 6, 8]
+
+# если нужны нечетные числа
+print(my_filter(numbers=numbers, function=lambda number: number % 2 != 0))  # [1, 3, 5, 7]
+
+# если нужны чсла больше 4
+print(my_filter(numbers=numbers, function=lambda number: number > 4))  # [5, 6, 7, 8]
