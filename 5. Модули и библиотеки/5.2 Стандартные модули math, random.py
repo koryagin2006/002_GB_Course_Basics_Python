6ae7fb4aabb537c2fbd21d68b22efcfa9dd05832
koryagin2006 <53011800+koryagin2006@users.noreply.github.com>
2020-01-18 02:27:50 +0500

    Update 5.2 Стандартные модули math, random.py

--- 5. Модули и библиотеки/5.2 Стандартные модули math, random.py
+++ 5. Модули и библиотеки/5.2 Стандартные модули math, random.py
@@ -41,3 +41,20 @@ print('----')
 #  - sample - список длиной k из последовательности
 #  - и многие другие
 
+from random import randint, choice, sample, shuffle
+
+# 1. загадать случайное число от 0 до 100
+print(randint(0, 100))
+
+# 2. выбрать победителя лотереи из списка players
+players = ['Max', 'Leo', 'Kate', 'Ron', 'Bill']
+print(choice(players))
+
+# 3. выбрать 3-х победителей из списка players
+print(sample(players, 3))
+
+# 4. перемешать карты в стопке (списке) cards
+cards = ['6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A']
+print(cards)
+shuffle(cards)
+print(cards)
