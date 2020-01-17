78dea6495eb87cc014ccbe45929f680423bdf11c
koryagin2006 <53011800+koryagin2006@users.noreply.github.com>
2020-01-18 01:29:46 +0500

    Update 4.6.2 Функции sorted, filter, map (filter).py

--- 4. Функции/4.6.2 Функции sorted, filter, map (filter).py
+++ 4. Функции/4.6.2 Функции sorted, filter, map (filter).py
@@ -3,3 +3,22 @@
 # filter(function, iterable)
 # аргументы: функция фильтрации, последовательность
 
+# набор чисел
+numbers = (1, 2, 3, 4, 5, 6, 7, 8)
+
+
+# получить только четные числа
+def is_even(number):
+    return number % 2 == 0
+
+
+result = filter(is_even, numbers)
+print(result)
+print(list(result))
+print('----')
+# набор строк
+names = ['Max', 'Leo', 'Kate']
+
+# получить строки больше 3 символов
+# print(filter(names, ))
+print(list(filter(lambda name: len(name)>3, names)))  # ['Kate']
