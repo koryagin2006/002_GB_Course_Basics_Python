e4ab8a53c8a8d7a2b1b8b4c2dc5d0a896ff0e7aa
koryagin2006 <53011800+koryagin2006@users.noreply.github.com>
2020-01-18 23:59:20 +0500

    Update 5.3.0 Создание собственных модулей.py
    Импорт собственных модулей

--- 5. Модули и библиотеки/5.3.0 Создание собственных модулей.py
+++ 5. Модули и библиотеки/5.3.0 Создание собственных модулей.py
@@ -4,3 +4,19 @@
 # import first_module
 # import folder.second_module
 
+import mod_a
+
+print(mod_a.foo)  # foo A
+mod_a.bar()  # bar A
+
+from folder_b.mod_b import foo, bar
+
+print(foo)  # foo B
+bar()  # bar B
+print('----')
+
+# Модули со скриптами
+#  - При любом варианте импорта скрипты будут выполняться
+#  - Если не указано никаких условий (if __name__ == ‘__main__’)
+#  - Это обязательно нужно учитывать при импорте
+
