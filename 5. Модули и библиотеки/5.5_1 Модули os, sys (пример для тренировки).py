09b388928fbc27901652176f611780743c0688ec
koryagin2006 <53011800+koryagin2006@users.noreply.github.com>
2020-01-20 00:05:57 +0500

    Update 5.5_1 Модули os, sys (пример для тренировки).py

--- 5. Модули и библиотеки/5.5_1 Модули os, sys (пример для тренировки).py
+++ 5. Модули и библиотеки/5.5_1 Модули os, sys (пример для тренировки).py
@@ -1,13 +1,14 @@
 # Пример для тренировки:
 #   В папке с модулем создать 5 подпапок, названия которых состоят из платформы,
-#   на которой запущен интерпретатор и порядкового номера, начиная с 1: win32_1, win_32_2, … Платформа может быть другой.
+#   на которой запущен интерпретатор и порядкового номера,
+#   начиная с 1: win32_1, win_32_2, … Платформа может быть другой.
 
 import sys, os
 
 name = sys.platform
 
-os.makedirs('{}\\{}'.format(os.getcwd(),'5.5_1 Пример'),exist_ok=True)
+os.makedirs('{}\\{}'.format(os.getcwd(), '5.5_1 Пример'), exist_ok=True)
 
 for i in range(1, 6):
     new_path = os.path.join('{}\\{}'.format(os.getcwd(), '5.5_1 Пример'), '{}_{}'.format(name, i))
-    os.makedirs(new_path,exist_ok=True)
+    os.makedirs(new_path, exist_ok=True)
