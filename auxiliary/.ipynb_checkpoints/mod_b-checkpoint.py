b4bed11f93513eb8559ce47463e49e0fa1bf3b0e
Andrey Koryagin <53011800+koryagin2006@users.noreply.github.com>
2020-03-23 15:01:22 +0500

    разное

---
+++ auxiliary/.ipynb_checkpoints/mod_b-checkpoint.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+foo = 'foo B'
+
+def bar():
+    print('bar B')
+
---
+++ auxiliary/.ipynb_checkpoints/mode_c-checkpoint.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+foo = 'foo C'
+
+if __name__ == ‘__main__’:
+    print('Я выполняюсь всегда')
+    print('Когда меня импортируют')
+    print('Ну или почти всегда')
